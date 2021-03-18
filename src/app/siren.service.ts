714cb8bf44962a6b745bd778824f5ab9dcf97f34
André Kervella <a.kervella@laposte.net>
2021-03-18 19:17:18 +0100

    test build

--- src/app/siren.service.ts
+++ src/app/siren.service.ts
@@ -7,13 +7,15 @@ import './sireninfo';
 @Injectable()
 export class SirenService {
   //baseUrl = 'http://editools.ddns.net/sirene/api/';
-  baseUrl = 'https://powertools35.freeboxos.fr/sirene/api/';
+  //baseUrl = 'https://powertools35.freeboxos.fr/sirene/api/';
+  baseUrl = 'https://yellow-dune-089695710.azurestaticapps.net/api/sirene';
 
   constructor(private http: Http) { }
 
   public getSirenInfo(siren: string): Observable<SirenInfo> {
     console.log('getSirenInfo(' + siren + ')');
     return this.http.get(`${this.baseUrl}values?siren=${siren}`)
-      .map(res => <SirenInfo>res.json() );
+      .map(res => <SirenInfo>res.json());
+      //.catch(err => {console.log('error getSirenInfo'); } );
   }
 }
